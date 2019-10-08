b528fb409435aaa561c6d9978742a12b913c39a5
blablamedia <pierre@gambaud.com>
2019-10-08 21:36:16 +0200

    vikingAttack(): almost done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,91 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return `Odin Owns You All!`;
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        
+        if(this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+        let randomIndexViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+        this.vikingAttacker = this.vikingArmy[randomIndexViking];
+        this.saxonDefender = this.saxonArmy[randomIndexSaxon];
+
+        this.saxonDefender.receiveDamage(this.vikingAttacker.attack());
+
+        if(this.saxonDefender <= 0) {
+            // en cours :)
+        }
+    }
+
+    saxonAttack() {
+
+    }
+
+    showStatus() {}
+
+}
