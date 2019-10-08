c7eda0543c3ff5a52b5a959d36f19176287ff3d8
martinezzzzzzz <55624848+martinezzzzzzz@users.noreply.github.com>
2019-10-08 21:34:26 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,25 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength)  {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name) {
+        this.name = name;
+        super(this.health, this.strength);
+    }
+}
 
 // Saxon
 class Saxon {}
