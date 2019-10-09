39f0bf132ffc63f225d1763536afe95b1777e316
Mira <myrahosd@hotmail.fr>
2019-10-09 12:11:25 +0200

    until the bonus / vikingAttack() method

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,81 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
 
-// Viking
-class Viking {}
+    attack(){
+        return this.strength;
+    }
 
-// Saxon
-class Saxon {}
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+    }
+
+}
+
+class Viking extends Soldier{
+        constructor(name, health, strenght){
+            super(name, health, strenght)
+            this.name = name;
+            this.health = health;
+            this.strength = strenght;
+        }
+
+        receiveDamage(theDamage){
+            this.health -= theDamage;
+            if (this.health > 0){
+                return `${this.name} has received ${theDamage} points of damage`;
+            } else if (this.health <= 0){
+                return `${this.name} has died in act of combat`;
+                
+            }
+        }
+
+        battleCry(){
+            return  "Odin Owns You All!";
+        }
+}
+
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength)
+        this.health = health; 
+        this.strength = strength;
+    }
+
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if (this.health > 0){
+            return `A Saxon has received ${theDamage} points of damage`;
+        } else if (this.health <= 0){
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack(){
+        //let randomSaxon = this.saxon[Math.floor(Math.random()*this.saxon.length)];
+    }
+        
+         
+    
+}
+
+
