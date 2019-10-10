175f22f72329fc34d6de81478478ea192941aa31
Sofy-Prd <sofy@DESKTOP-4LRCARK.localdomain>
2019-10-10 16:49:54 +0200

    second commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -50,6 +50,8 @@ class War {
   constructor() {
     this.vikingArmy = [];
     this.saxonArmy = [];
+    
+
   }
   addViking(vik) {
     this.vikingArmy.push(vik);
@@ -58,17 +60,40 @@ class War {
     this.saxonArmy.push(sax);
   }
   vikingAttack() {
-    this.saxonArmy[
-      Math.floor(Math.random() * this.saxonArmy.length)
-    ].receiveDamage(
-      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
-        .strength
-    );
+    this.saxA = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    this.vikA = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    this.saxA.receiveDamage(this.vikA.strength);
 
-    // if (this.saxonArmy[0].health >= 0) {
-    //   this.saxonArmy.splice[0];
-    // }
+    if (this.saxA.health <= 0) {
+      this.saxonArmy=this.saxonArmy.splice(this.saxonArmy.indexOf(this.saxA),1);
+     
   }
-  saxonAttack() {}
-  showStatus() {}
+  return this.saxonArmy;
+  }
+
+  saxonAttack() {
+    this.saxB = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    this.vikB = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+    this.vikB.receiveDamage(this.saxB.strength);
+
+    if (this.vikB.health <= 0) {
+      this.vikingArmy=this.vikingArmy.splice(this.vikingArmy.indexOf(this.vikB),1);
+     
+  }
+  return this.vikingArmy;
+
+  }
+  showStatus() {
+    if (this.saxonArmy.length===0){
+    return "Vikings have won the war of the century!";
+  } else if(this.vikingArmy.length===0){
+    return "Saxons have fought for their lives and survive another day...";
+  } else if (this.saxonArmy.length>0 && this.vikingArmy.length>0){
+    return "Vikings and Saxons are still in the thick of battle."           }
 }
+}
+
+
+
+
