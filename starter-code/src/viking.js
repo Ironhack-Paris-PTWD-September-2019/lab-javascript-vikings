5539153a9b17b75046fb9c46ab77b070bb2908de
Emmanuelle Blancardi <emmanuelleblancardi@MacBook-Pro-de-Emmanuelle.local>
2019-10-10 18:17:06 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  }
+  attack(){
+     return this.strength
+  }
+  receiveDamage(damage) {
+    this.health = this.health-damage;
+  }
+}
 
-// Viking
-class Viking {}
+// Vik
+
+class Viking extends Soldier {
+  constructor (health, strength, name){
+    super(health, strength)
+
+    this.name = name;
+  }
+}
+
+if this.health >= 1 {
+  console.log(`${this.name} has received ${this.damage} points of damage`);
+}
+
+if this.health <1 {
+  console.log(`${this.name} has died in act of combat`);
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
